3c324775b0b2ae6b8bc99c70b02a06a6264dc153
DESKTOP-0N9Q9LH\a <ingunbi@gmail.com>
2023-01-16 14:20:41 +0900

    프로젝트 생성2

--- config/settings.py
+++ config/settings.py
@@ -114,7 +114,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [ BASE_DIR / 'static']
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
